3d80f5f613d8e158e75d970d8bb32de811b8e4f4
leothesouthafrican <38779415+leothesouthafrican@users.noreply.github.com>
2022-05-24 18:11:44 +0100

    finish individual add NN class

--- genetics.py
+++ genetics.py
@@ -1,63 +1,123 @@
 from dis import dis
+from msilib.schema import Class
 from random import shuffle, choice, sample, random
 from operator import attrgetter
 from copy import deepcopy
 import numpy as np
+import math
 
+class Environment:
+    def __init__(self,apple_position = None, borders = [], environment_size = 20):
+        self.apple_position = apple_position
+        self.borders = borders
+        self.environment_size = environment_size
+
+        if self.apple_position is None:
+            self.apple_position = np.random.randint(low=1, high=self.environment_size, size=2)
+        
+        if self.borders == []:
+            for x in range(0,self.environment_size):
+                if x == 0:
+                    for y in range(0,self.environment_size):
+                        self.borders.append([x,y])
+                if x == (self.environment_size - 1) and y != 0:
+                    for y in range(0,self.environment_size):
+                        self.borders.append([x,y])
+                for y in range(0,self.environment_size):
+                    if y == (self.environment_size - 1) and x != 0:
+                        self.borders.append([x,y])
+                    elif y == 0 and x != 0:
+                        self.borders.append([x,y])
+        
 class Individual:
     def __init__(
         self,
-        matrix_weights = None,
-        snake_head_coordinates = [1,1],
-        heading = "E",
+        current_environment,
+        matrix_weights_1 = None,
+        matrix_weights_2 = None,
+        bias_vector = None,
+        snake_head_coordinates = None,
+        heading = "N",
         occupied_blocks = [(10,10),(10,9)],
         length = None,
         relative_position = []
     ):
-        if matrix_weights is None:
-            self.matrix_weights = np.random.rand(4,5)
 
+        if matrix_weights_1 is None:
+            self.matrix_weights_1 = np.random.rand(4,5)
+        if matrix_weights_2 is None:
+            self.matrix_weights_2 = np.random.rand(5,3)
+        if bias_vector is None:
+            self.bias_vector = np.random.rand(1,3)
+
+        self.environment = current_environment
         self.snake_head_coordinates = snake_head_coordinates
         self.heading = heading
         self.occupied_blocks = occupied_blocks
         self.length = len(self.occupied_blocks)
         self.relative_position = relative_position
 
+        if self.snake_head_coordinates is None:
+            self.snake_head_coordinates = np.random.randint(low=1, high=self.environment.environment_size, size=2)
+
+
     def distance_computer(self):
 
-        print("Works!")
-        if self.heading == "N":
-            print("If Works!")
-            distance_left = self.snake_head_coordinates[0]
-            distance_right = 19 - self.snake_head_coordinates[0]
-            distance_forward = 19 - self.snake_head_coordinates[1]
+        #Loading environment into method for easier access
+        environment = self.environment
 
-            self.relative_position = [distance_left,distance_forward,distance_right]
+        #Calculating sin angle from snake_head_coordinates (taking into account heading) to apple_position
+        radians = math.atan2(np.abs(self.snake_head_coordinates[1]-environment.apple_position[1]), np.abs(self.snake_head_coordinates[0]- environment.apple_position[0]))
+        degrees = math.degrees(radians)
+
+        if self.heading == "N":
+            distance_left = self.snake_head_coordinates[0]
+            distance_right = (environment.environment_size - 1) - self.snake_head_coordinates[0]
+            distance_forward = (environment.environment_size - 1) - self.snake_head_coordinates[1]
+
+            self.relative_position = [distance_left,distance_forward,distance_right, round(np.sin(degrees),2)]
             
 
         elif self.heading == "E":
-            distance_left = 19 - self.snake_head_coordinates[1]
+            distance_left = (environment.environment_size - 1) - self.snake_head_coordinates[1]
             distance_right = self.snake_head_coordinates[1]
-            distance_forward = 19 - self.snake_head_coordinates[0]
+            distance_forward = (environment.environment_size - 1) - self.snake_head_coordinates[0]
 
-            self.relative_position = [distance_left, distance_forward, distance_right]
+            #Adjusting angle for heading
+            degrees += 270
+            
+            self.relative_position = [distance_left, distance_forward, distance_right, round(np.sin(degrees),2)]
 
         elif self.heading == "S":
-            distance_left = 19 - self.snake_head_coordinates[0]
+            distance_left = (environment.environment_size - 1) - self.snake_head_coordinates[0]
             distance_right = self.snake_head_coordinates[0]
             distance_forward = self.snake_head_coordinates[1]
 
-            self.relative_position = [distance_left, distance_forward, distance_right]
+            #Adjusting angle for heading
+            degrees += 180
+
+            self.relative_position = [distance_left, distance_forward, distance_right, round(np.sin(degrees),2)]
 
         elif self.heading == "W":
             distance_left = self.snake_head_coordinates[1]
-            distance_right = 19 - self.snake_head_coordinates[1]
+            distance_right = (environment.environment_size - 1) - self.snake_head_coordinates[1]
             distance_forward = self.snake_head_coordinates[0]
 
-            self.relative_position = [distance_left, distance_forward, distance_right]
+            #Adjusting angle for heading
+            degrees += 90
+
+            self.relative_position = [distance_left, distance_forward, distance_right, round(np.sin(degrees),2)]
 
     def __str__(self):
            return f"Current relative position: {self.distance_computer}"
+    
+    class NN_engine:
+        def __init__(self, individual):
+            self.individual = individual
+
+        def softmax(x):
+            e_x = np.exp(x)
+            return e_x/ e_x.sum()
 
 class Population:
     def __init__(self, size, optim, **kwargs):
--- snake_main.py
+++ snake_main.py
@@ -1,5 +1,6 @@
 import numpy as np
-from genetics import Individual
+import math
+from genetics import Individual, Environment, Population
 
 from random import random
 from copy import deepcopy
@@ -7,12 +8,19 @@ from operator import attrgetter
 
 def softmax(x):
     e_x = np.exp(x)
-    return e_x / e_x.sum()
+    return e_x/ e_x.sum()
 
-
-snake_1 = Individual()
+environment_1 = Environment(environment_size=15, apple_position = [13,13])
+snake_1 = Individual(environment_1, snake_head_coordinates=[10,10], heading="E")
 
 snake_1.distance_computer()
-print(snake_1.heading)
 
-print(snake_1.relative_position)
+hidden_layer_1 = np.dot(snake_1.relative_position, snake_1.matrix_weights_1)
+
+pre_bias_output = np.dot(hidden_layer_1,snake_1.matrix_weights_2)
+
+bias_added_vector = snake_1.bias_vector + pre_bias_output
+
+final_output = softmax(bias_added_vector)
+
+print(final_output)

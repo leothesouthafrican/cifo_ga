e414753704bcbc5f3a6ce4876d9e05b6932c0faf
leothesouthafrican <38779415+leothesouthafrican@users.noreply.github.com>
2022-05-27 10:31:06 +0100

    last safe save

--- snake_main.py
+++ snake_main.py
@@ -1,13 +1,21 @@
 import numpy as np
 import math
-from genetics import Individual, Environment, NN_Engine
+from genetics import Individual, Environment, NN_Engine, Population
 
 from random import random
 from copy import deepcopy
 from operator import attrgetter
 
 
-environment_1 = Environment(environment_size=10)
+environment_1 = Environment(environment_size=8)
+
+
+population_1 = Population( size = 10, environment_used = environment_1, optim = "max")
+
+for i in population_1.individuals:
+    print(i.representation)
+
+
 snake_1 = Individual(environment_1, snake_head_coordinates = None, heading="N")
 engine_1 = NN_Engine(snake_1,environment_1)
 
@@ -23,6 +31,4 @@ while snake_1.available_epochs > 0:
     print(environment_1.__str__())
     print(snake_1.__str__())
     print("+++++++")
-
-
-print(snake_1.__str__())
+print(snake_1.create_representation())

a048322c670162ffd45265ac24f562caa6570c8d
leothesouthafrican <38779415+leothesouthafrican@users.noreply.github.com>
2022-06-08 19:52:56 +0100

    add aggregator to analysis

--- analysis.py
+++ analysis.py
@@ -7,33 +7,42 @@ class Analysis:
     def __init__(self, population):
         self.population = population
     #Function that computes average value per epoch across n runs
-    def average_value_per_epoch(self, path, metric, population):
+    def average_value_per_epoch(self, path, metrics, population):
         
         #List of all files in the directory
         file_list = os.listdir(path)
         #Iterate through all excels
+        analysis_dict = {}
         for file in file_list:
             print(f"Current File: {file}")
-            full_column = []
-            for generation in range(population.gens):
-                print(f"Current Generation: {generation}")
-                #Storing all sheets in each excel file
-                wb = openpyxl.load_workbook(path + file)
-                #Getting number of sheets contained within excel
-                total_metric = 0
-                for sheet in wb.sheetnames:
-                    print(f"Current Sheet: {sheet}")
-                    temp_sheet = pd.read_excel(path + file, sheet_name= sheet) 
+            combined_df = pd.DataFrame()
+            for metric in metrics:
+                full_column = []
+                for generation in range((population.gens)):
+                    print(f"Current Generation: {generation}")
                     
-                    value_to_add = int(temp_sheet.iloc[generation,metric])
-                    print(f"Value Added: {value_to_add}")
-                    total_metric += value_to_add
-                average_metric = total_metric/len(wb.sheetnames)
-                print(f"Average Metric: {average_metric}")
-                full_column.append(average_metric)
-            print(f"Full Column: {full_column}")
+                    #Getting number of sheets contained within excel
+                    total_metric = 0
+                    for run in range(population.runs):
+                        print(f"Current Sheet: {run}")
+                        temp_sheet = pd.read_excel(path + file, sheet_name= "Run_" + str(run)) 
+                        print(f"Generation: {generation}")
+                        value_to_add = int(temp_sheet.iloc[generation,metric])
+                        print(f"Value Added: {value_to_add}")
+                        total_metric += value_to_add
+                    average_metric = total_metric/population.runs
+                    print(f"Average Metric: {average_metric}")
+                    full_column.append(average_metric)
+                print(f"Full Column: {full_column}")
+                combined_df[metric] = full_column
+                print(combined_df)
+            combined_df.to_excel(path + "averaged_" + file)
+            print(analysis_dict)
+
+
+
 if __name__ == "__main__":
     environment_1 = Environment(environment_size=15)
-    population_1 = Population(size = 50, gens = 500,environment_used = environment_1, optim = "max", output_file_name = "test_4", individual_moves=750)
+    population_1 = Population(size = 50, gens = 5,runs = 5,environment_used = environment_1, optim = "max", output_file_name = "test_4", individual_moves=750)
     analysis_1 = Analysis(population_1)
-    analysis_1.average_value_per_epoch('/Users/leo/Desktop/nova/optimisation/other/cifo_ga/test_set_1/', 1, population_1)
+    analysis_1.average_value_per_epoch('/Users/leo/Desktop/nova/optimisation/other/cifo_ga/analysis_test/', [1,3,4,5,6,7], population_1)
